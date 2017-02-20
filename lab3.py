1df2e9c63c743483910ed0b75f77a15a471b0189
AntonBorkivskyy <borkivskyy@ucu.edu.ua>
2017-02-21 01:05:49 +0200

    Few updates of some methods

--- lab3.py
+++ lab3.py
@@ -22,10 +22,12 @@ class Property:
 class Apartment(Property):
     valid_laundries = ("coin", "ensuite", "none")
     valid_balconies = ("yes", "no", "solarium")
+
     def __init__(self, balcony='', laundry='', **kwargs):
         super().__init__(**kwargs)
         self.balcony = balcony
         self.laundry = laundry
+
     def display(self):
         super().display()
         print("APARTMENT DETAILS")
